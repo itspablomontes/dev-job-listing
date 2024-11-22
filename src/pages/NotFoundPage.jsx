db8369f2ee9d7448ebe722ba9113fb2b6fa04e71
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2024-11-22 18:09:27 -0300

    feat: Add custom 404 page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const NotFoundPage = () => {
+  return (
+    <>
+      <section className="text-center flex flex-col justify-center items-center h-96">
+        <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
+        <h1 className="text-6xl font-bold mb-4"></h1>
+        <p className="text-xl mb-5"></p>
+        <Link
+          to="/"
+          className="text-white bg-blue-700 hover:bg-blue-900 rounded-md px-3 py-2 mt-4"
+        >
+          Go Back
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default NotFoundPage;
